a837c5b63e3799955d6c461db14b53b7c37c88c8
Thiago Guimarães <thiagogsr@gmail.com>
2018-02-19 22:32:15 -0300

    Install dht22 node sensor

---
+++ dht22/ping.js
@@ -0,0 +1,12 @@
+var sensor = require('node-dht-sensor');
+
+const sensorType = 22;
+const sensorPin = 24;
+
+sensor.read(sensorType, sensorPin, function(err, temperature, humidity) {
+  if (!err) {
+    console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+                'humidity: ' + humidity.toFixed(1) + '%'
+    );
+  }
+});
